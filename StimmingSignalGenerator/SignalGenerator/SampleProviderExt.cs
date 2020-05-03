c18d087f57ad4737b67ce910bee1b2e017619697
Haiyaku <haiyaku@tuta.io>
2020-05-03 23:23:53 +0700

    Remove unuse old AM FM code

--- StimmingSignalGenerator/SignalGenerator/SampleProviderExt.cs
+++ StimmingSignalGenerator/SignalGenerator/SampleProviderExt.cs
@@ -10,17 +10,6 @@ namespace StimmingSignalGenerator.SignalGenerator
 {
    public static class SampleProviderExt
    {
-      public static ISampleProvider AddAM(
-         this ISampleProvider SourceSampleProvider,
-         ISampleProvider AmSampleProvider)
-         => new AmplitudeModulationProvider(SourceSampleProvider, AmSampleProvider);
-
-      public static ISampleProvider AddFM(
-         this ISampleProvider SourceSampleProvider,
-         ISampleProvider FmSampleProvider,
-         float pitchOctaveUpDown = 1)
-         => new FrequencyModulationProvider(SourceSampleProvider, FmSampleProvider, pitchOctaveUpDown);
-
       public static ISampleProvider Gain(
          this ISampleProvider SourceSampleProvider,
          Func<float, float> gainFuntion)
