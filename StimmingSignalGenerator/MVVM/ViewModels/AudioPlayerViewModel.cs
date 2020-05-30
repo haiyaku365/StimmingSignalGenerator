90d88d314d02534cf85559abb451e5a64d8eec07
Haiyaku <haiyaku@tuta.io>
2020-05-31 04:42:40 +0700

    Play, stop button enable disable with AppState.IsPlaying

--- StimmingSignalGenerator/MVVM/ViewModels/AudioPlayerViewModel.cs
+++ StimmingSignalGenerator/MVVM/ViewModels/AudioPlayerViewModel.cs
@@ -40,8 +40,11 @@ namespace StimmingSignalGenerator.MVVM.ViewModels
          AppState = Locator.Current.GetService<AppState>();
 
          audioPlayer = new AudioPlayer(sampleProvider);
-         PlayCommand = ReactiveCommand.Create(() => Play()).DisposeWith(Disposables);
-         StopCommand = ReactiveCommand.Create(() => Stop()).DisposeWith(Disposables);
+
+         PlayCommand = ReactiveCommand.Create(() => Play(), AppState.WhenAnyValue(x => x.IsPlaying, x => !x))
+            .DisposeWith(Disposables);
+         StopCommand = ReactiveCommand.Create(() => Stop(), AppState.WhenAnyValue(x => x.IsPlaying))
+            .DisposeWith(Disposables);
       }
 
       public void Play()
